12c01d5f6fd66178cae985536f9852d21ce84458
NeilWong <nwong1250@gmail.com>
2014-10-22 11:28:34 -0700

    Neil's Starfield

--- build-tmp/source/Starfield.java
+++ build-tmp/source/Starfield.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Starfield extends PApplet {
+
 /*
 NormalParticle star;
 //OddballParticle [] ster;
@@ -83,7 +99,7 @@ OddballParticle()
 
 
 Particle [] dots = new Particle[1000];
-void setup()
+public void setup()
 {
 	size(640,640);
 	for (int i = 0; i<dots.length; i++)
@@ -97,7 +113,7 @@ void setup()
 	dots[0] = new OddballParticle();
 	//dots[1] = new LargeParticle();
 }
-void draw()
+public void draw()
 {	 
 	background(0);
 	for (int i = 0; i<dots.length; i++)
@@ -204,3 +220,12 @@ class LargeParticle extends NormalParticle implements Particle
 		y=mouseY;
 	}
 }}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Starfield" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
